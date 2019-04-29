1b3f2a4d9ddd1a6d3991615eb900f09f9d0e4998
Levin Gerdes <levin.gerdes@esa.int>
2019-04-29 18:03:35 +0200

    Auto formatting

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -6,28 +6,20 @@ using namespace path_planning;
 using namespace Eigen;
 namespace LM = locomotion_switcher;
 
-Task::Task(std::string const& name)
-    : TaskBase(name),
-      mEnv(NULL)
-{
-}
+Task::Task(std::string const& name) : TaskBase(name), mEnv(NULL) {}
 
 Task::Task(std::string const& name, RTT::ExecutionEngine* engine)
-    : TaskBase(name, engine),
-      mEnv(NULL)
+    : TaskBase(name, engine), mEnv(NULL)
 {
 }
 
-Task::~Task()
-{
-}
+Task::~Task() {}
 
 bool Task::configureHook()
 {
-    if (! TaskBase::configureHook())
-        return false;
-	
-  // Take Information relative to Cost
+    if (!TaskBase::configureHook()) return false;
+
+    // Take Information relative to Cost
     slope_values = _slope_values.get();
     locomotion_modes = _locomotion_modes.get();
     cost_data = _cost_data.get();
@@ -39,33 +31,39 @@ bool Task::configureHook()
 
 bool Task::startHook()
 {
-    if (! TaskBase::startHook())
+    if (!TaskBase::startHook())
+    {
         return false;
-	
+    }
+
     elevationMatrix = readMatrixFile(_elevationFile.get());
 
     if (!_traversability_map.connected())
     {
-        LOG_DEBUG_S << "traversability map input is not connected, a predefined map will be used instead";
+        LOG_DEBUG_S
+            << "traversability map input is not connected, a predefined map will be used instead";
         costMatrix = readMatrixFile(_localCostFile.get());
     }
 
     globalCostMatrix = readMatrixFile(_globalCostFile.get());
 
-	
-    planner = new PathPlanning_lib::PathPlanning(cost_data, slope_values, locomotion_modes, risk_distance, reconnect_distance, risk_ratio);
+    planner = new PathPlanning_lib::PathPlanning(
+        cost_data, slope_values, locomotion_modes, risk_distance, reconnect_distance, risk_ratio);
 
-  //pos is the global offset of the Global Map relative to World Frame
-  //TODO: set pos externally (and change its name to globalOffset maybe)
+    // pos is the global offset of the Global Map relative to World Frame
+    // TODO: set pos externally (and change its name to globalOffset maybe)
     base::Pose2D pos;
     pos.position[0] = 0.0;
     pos.position[1] = 0.0;
-	
-	
-    planner->initGlobalMap(1.0, _local_res, pos, elevationMatrix, globalCostMatrix);  //TODO: 1.0 should be a configurable parameter, maybe global_res
 
+    planner->initGlobalMap(
+        1.0,
+        _local_res,
+        pos,
+        elevationMatrix,
+        globalCostMatrix);  // TODO: 1.0 should be a configurable parameter, maybe global_res
 
-  // Initializing goalWaypoint and wRover
+    // Initializing goalWaypoint and wRover
     goalWaypoint.position[0] = 0;
     goalWaypoint.position[1] = 0;
     currentGoal.position[0] = 0;
@@ -82,12 +80,12 @@ void Task::updateHook()
 {
     TaskBase::updateHook();
 
-  // Goal Waypoint
+    // Goal Waypoint
     if (_goalWaypoint.read(goalWaypoint) == RTT::NewData)
     {
 
-        if ((goalWaypoint.position[0] != currentGoal.position[0]) ||
-           (goalWaypoint.position[1] != currentGoal.position[1]))
+        if ((goalWaypoint.position[0] != currentGoal.position[0])
+            || (goalWaypoint.position[1] != currentGoal.position[1]))
         {
             if (planner->setGoal(goalWaypoint))
             {
@@ -97,12 +95,11 @@ void Task::updateHook()
         }
     }
 
-  // Rover Pose
+    // Rover Pose
     if (_pose.read(pose) == RTT::NewData)
     {
         wRover.position = pose.position;
         wRover.heading = pose.getYaw();
-
 
         if (state == GLOBAL_PLANNING)
         {
@@ -111,15 +108,17 @@ void Task::updateHook()
             trajectory = planner->getNewPath(wRover);
             _trajectory.write(trajectory);
             trajectory2D = trajectory;
-            for(uint i = 0; i<trajectory2D.size(); i++)
+            for (uint i = 0; i < trajectory2D.size(); i++)
+            {
                 trajectory2D[i].position[2] = 0;
+            }
             _trajectory2D.write(trajectory2D);
             _global_Total_Cost_map.write(planner->getGlobalTotalCostMap());
             _global_Cost_map.write(planner->getGlobalCostMap());
             state = PATH_COMPUTED;
         }
 
-        if ((state == PATH_COMPUTED)||(state == LOCAL_PLANNING))
+        if ((state == PATH_COMPUTED) || (state == LOCAL_PLANNING))
         {
             LM::LocomotionMode lm;
             std::string loc = planner->getLocomotionMode(wRover);
@@ -131,56 +130,60 @@ void Task::updateHook()
             _actual_total_cost.write(planner->getTotalCost(wRover));
         }
     }
-	
-  // Traversability Map
+
+    // Traversability Map
 
     if (state == PATH_COMPUTED)
     {
         if (!_traversability_map.connected())
         {
-            if (planner->computeLocalPlanning(wRover, costMatrix, _local_res, trajectory, _keep_old_waypoints))
+            if (planner->computeLocalPlanning(
+                    wRover, costMatrix, _local_res, trajectory, _keep_old_waypoints))
             {
                 _trajectory.write(trajectory);
                 trajectory2D = trajectory;
-                for(uint i = 0; i<trajectory2D.size(); i++)
-                    trajectory2D[i].position[2] = 0;
+                for (uint i = 0; i < trajectory2D.size(); i++) trajectory2D[i].position[2] = 0;
                 _trajectory2D.write(trajectory2D);
             }
         }
         else if (_traversability_map.read(traversability_map) == RTT::NewData)
         {
-            // LOG_DEBUG_S << "Starting Traversability map reading loop, period loop is set as" << TaskContext::getPeriod();
-            if(planner->computeLocalPlanning(wRover, traversability_map, _local_res, trajectory, _keep_old_waypoints))
+            // LOG_DEBUG_S << "Starting Traversability map reading loop, period loop is set as" <<
+            // TaskContext::getPeriod();
+            if (planner->computeLocalPlanning(
+                    wRover, traversability_map, _local_res, trajectory, _keep_old_waypoints))
             {
                 _trajectory.write(trajectory);
                 trajectory2D = trajectory;
-                for(uint i = 0; i<trajectory2D.size(); i++)
-                    trajectory2D[i].position[2] = 0;
+                for (uint i = 0; i < trajectory2D.size(); i++) trajectory2D[i].position[2] = 0;
                 _trajectory2D.write(trajectory2D);
             }
             _local_Risk_map.write(planner->getLocalRiskMap(wRover));
             _local_Propagation_map.write(planner->getLocalPropagationMap(wRover));
             _finished_planning.write(true);
-            // LOG_DEBUG_S << "Finishing Traversability map reading loop, period loop is set as" << TaskContext::getPeriod();
+            // LOG_DEBUG_S << "Finishing Traversability map reading loop, period loop is set as" <<
+            // TaskContext::getPeriod();
         }
     }
 }
 
-
-std::vector< std::vector<double> > Task::readMatrixFile(std::string map_file)
+std::vector<std::vector<double>> Task::readMatrixFile(std::string map_file)
 {
     LOG_DEBUG_S << "Reading map " << map_file;
-    std::vector< std::vector<double> > mapMatrix;
+    std::vector<std::vector<double>> mapMatrix;
     std::string line;
     std::ifstream eFile(map_file.c_str(), std::ios::in);
     double Nrow = 0, Ncol = 0;
-    std::vector <double> row;
+    std::vector<double> row;
 
-    if( eFile.is_open() ){
-        while ( std::getline(eFile, line) ){
+    if (eFile.is_open())
+    {
+        while (std::getline(eFile, line))
+        {
             std::stringstream ss(line);
             std::string cell;
-           while( std::getline(ss, cell, ' ') ){
+            while (std::getline(ss, cell, ' '))
+            {
                 double val;
                 std::stringstream numericValue(cell);
                 numericValue >> val;
@@ -191,27 +194,19 @@ std::vector< std::vector<double> > Task::readMatrixFile(std::string map_file)
             row.clear();
             Nrow++;
         }
-    eFile.close();
+        eFile.close();
 
-    Ncol /= Nrow;
-        LOG_DEBUG_S << "PLANNER: Cost map of " << Ncol
-                  << " x "          << Nrow << " loaded.";
-    } else {
+        Ncol /= Nrow;
+        LOG_DEBUG_S << "PLANNER: Cost map of " << Ncol << " x " << Nrow << " loaded.";
+    }
+    else
+    {
         LOG_DEBUG_S << "PLANNER: Problem opening the file";
         return mapMatrix;
     }
     return mapMatrix;
 }
 
-void Task::errorHook()
-{
-    TaskBase::errorHook();
-}
-void Task::stopHook()
-{
-    TaskBase::stopHook();
-}
-void Task::cleanupHook()
-{
-    TaskBase::cleanupHook();
-}
+void Task::errorHook() { TaskBase::errorHook(); }
+void Task::stopHook() { TaskBase::stopHook(); }
+void Task::cleanupHook() { TaskBase::cleanupHook(); }
